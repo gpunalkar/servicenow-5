a8b8ddd8038d0e98f6afb430cab70b576b821e1c
Jeff Neslen <jeff.neslen@matrix42.com>
2012-07-10 12:05:33 -0600

    Added missing files to git

--- application/classes/controller/public/index.php
+++ application/classes/controller/public/index.php
@@ -4,12 +4,18 @@ class Controller_Public_Index extends Controller_Public {
 
 	public function action_index()
 	{
-		$action = $this->request->action();
-		//exit(\Debug::vars($action));
+
 	}
 
 	public function action_workplace_mobility()
 	{
-		echo 'bingo';
+
 	}
+
+	public function action_workplace_virtualization()
+	{
+
+	}
+
+
 }
