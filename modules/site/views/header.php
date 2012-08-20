f545d38a34894ee0b42ad7a2b7a15a4258d439aa
jneslen <jeff.neslen@matrix42.com>
2012-08-20 09:45:40 -0600

    Changed home logo link to link to main homepage

--- modules/site/views/header.php
+++ modules/site/views/header.php
@@ -5,6 +5,7 @@
  * @brief
  *
  */
+$home_url = \Helper::language() === 'de' ? 'http://www.matrix42.de' : 'http://www.matrix42.com';
 ?>
 <!-- Begin Header -->
 <div id="main-nav" class="navbar navbar-fixed-top">
@@ -12,7 +13,7 @@
 		<div class="container">
 			<div class="row">
 				<div id="logo" class="span2">
-					<a href="/"><img src="/assets/img/matrix42-logo.png" alt="Matrix42" /></a>
+					<a href="<?=$home_url?>"><img src="/assets/img/matrix42-logo.png" alt="Matrix42" /></a>
 				</div><!-- logo -->
 				<div id="logo2" class="span2 offset8">
 					<img src="/assets/img/servicenow-logo.png" alt="ServiceNow" />
