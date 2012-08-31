a2a23d5c55d688a513eb96e47806d5b7936d110b
jneslen <jeff.neslen@matrix42.com>
2012-08-31 16:41:34 -0600

    forgot the german index

--- application/views/de/promo/index.php
+++ application/views/de/promo/index.php
@@ -5,12 +5,16 @@
 				<?=$products?>
 			</div><!-- span 6 -->
 			<div class="span6">
-				<div class="tag-content row-fluid">
-					<h1>Wählen Sie Ihr Add-On Paket!</h1>
+				<div class="well hot center">
+					<h1 class="jumbo-title"><?=__('Exclusive for')?></h1>
+					<h1>Matrix42 <?=__('Customers')?>!</h1>
 				</div>
+				<h1>Wählen Sie Ihr Add-On Paket!</h1>
 				<?=$packages?>
-				<?=$instructions?>
 			</div><!-- span6 -->
+		</div><!-- row-fluid -->
+		<div class="row-fluid">
+			<?=$instructions?>
 		</div><!-- row-fluid -->
 	</div><!-- container -->
 </div><!-- main-container -->
