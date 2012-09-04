981d45ca483226e96313ed3da9d4b061dbbc87f3
jneslen <jeff.neslen@matrix42.com>
2012-09-04 14:50:11 -0600

    side lead form fix for DE

--- application/views/de/promo/instructions.php
+++ application/views/de/promo/instructions.php
@@ -1,10 +1,10 @@
 <div id="support" class="well plastic padded-top">
 	<h2>Fragen? Rufen Sie uns an!</h2>
 	<h3 class="emphasis">+49 (0)6102 816-0</h3>
-	<a href="#side-lead-form-anchor" class="btn btn-mini btn-inverse btn-icon scroll left"><span class="icon phone"></span>Rückruf anfordern!</a>
+	<a href="#side-lead-form-anchor" class="btn btn-mini btn-inverse btn-icon scroll left sidebar-lead" data-campaign="6"><span class="icon phone"></span>Rückruf anfordern!</a>
 	<div class="right"><?=$chat->render()?></div>
 	<div class="clear"></div>
-	<div id="main-lead-form" class="hidden padded-top">
+	<div id="side-lead-formc" class="hidden padded-top">
 		<?=$lead_form?>
 		<div class="clear"></div>
 	</div><!-- container -->
