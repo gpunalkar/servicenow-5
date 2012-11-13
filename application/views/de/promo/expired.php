1655fe66b92f5491833937d0011bbb151ea7fa3c
jneslen <jeff.neslen@matrix42.com>
2012-11-13 09:35:40 -0700

    fixed homepage anchor link on expired page for promo

--- application/views/de/promo/expired.php
+++ application/views/de/promo/expired.php
@@ -1,7 +1,7 @@
 <div class="container nav-pad">
 	<div class="row-fluid padded-top">
 		<div class="span12">
-			<h1>Diese Promotion ist nicht mehr gültig. Bitte besuchen Sie unsere Website <a href="http://www.matrix2.de">www.matrix42.de</a></h1>
+			<h1>Diese Promotion ist nicht mehr gültig. Bitte besuchen Sie unsere Website <a href="http://www.matrix42.de">www.matrix42.de</a></h1>
 		</div><!-- span6 -->
 	</div><!-- row-fluid -->
 </div><!-- container -->
